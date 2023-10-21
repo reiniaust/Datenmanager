cf283dc45d1ed89fd69bce92b5f8c47801a452ad
reiniaust <reinhard.austermeie@gmx.de>
2023-10-21 11:39:37 +0200

    Nur neu suchen, wenn Worte geändert wurden

--- main.py
+++ main.py
@@ -103,6 +103,9 @@ def save_data_to_db(view_name, data):
 views_and_data = load_data_from_db()
 
 last_view_name = ""
+last_search_words = ""
+last_found_views_and_data = {}
+last_view = {}
 
 
 @app.route("/start")
@@ -110,12 +113,12 @@ def index():
     # views = load_data_from_db()
 
     for view_name, view in views_and_data.items():
-        
+
         # Es wird nur die erste View verwendet (also break der Schleife nach dem render_template)
 
         columnsWidth = {}
         for column_name in views[view_name]["columns"].items():
-            columnsWidth[column_name] = "50"      
+            columnsWidth[column_name] = "50"
 
         view["data_found"] = view["data"]
 
@@ -130,6 +133,9 @@ def form():
     # views = load_data_from_db()
 
     global last_view_name
+    global last_search_words
+    global last_found_views_and_data
+    global last_view
 
     current_view_name = request.form['view_name']
 
@@ -137,84 +143,93 @@ def form():
     # Suchtext in Wörter aufteilen
     search_words = search.lower().split()
 
-    if len(search_words) == 0 and current_view_name == last_view_name:
+    if search_words == last_search_words and current_view_name == last_view_name:
         # wenn kein Suchwort definiert ist, dann neu einlesen
         local_views_and_data = load_data_from_db()
     else:
         local_views_and_data = views_and_data
 
-    last_view_name = current_view_name
+    found_in_current_view = True
 
-    found_in_current_view = False
+    if search_words == last_search_words:
+        found_views_and_data = last_found_views_and_data
+        view = last_view
 
-    views_found = {}
-    found_view_name = ""
-    found_key = ""
-    all_search_words = search_words
-    for view_name, view in local_views_and_data.items():
-        found_in_view = False
-        view["data_found"] = []
+    else:  # Suchen
 
-        if len(search_words) != 0:
-            # mit Suche am Wortanfang deaktiviert (12.10.) if re.compile(r"\b" + all_search_words[0]).search(str(view_name).lower()):
-            if re.compile(all_search_words[0]).search(str(view_name).lower()):
-                found_view_name = view_name
-                search_words = all_search_words[1:]
-            for key, value in view["data"][0].items():
-                 # mit Suche am Wortanfang deaktiviert (12.10.) if re.compile(r"\b" + all_search_words[0]).search(str(key).lower()):
-                if re.compile(all_search_words[0]).search(str(key).lower()):
-                    found_key = key
+        last_search_words = search_words
+
+        found_views_and_data = {}
+        found_view_name = ""
+        found_key = ""
+        all_search_words = search_words
+        for view_name, view in local_views_and_data.items():
+            found_in_view = False
+            view["data_found"] = []
+
+            if len(search_words) != 0:
+                # mit Suche am Wortanfang deaktiviert (12.10.) if re.compile(r"\b" + all_search_words[0]).search(str(view_name).lower()):
+                if re.compile(all_search_words[0]).search(str(view_name).lower()):
+                    found_view_name = view_name
                     search_words = all_search_words[1:]
+                for key, value in view["data"][0].items():
+                    # mit Suche am Wortanfang deaktiviert (12.10.) if re.compile(r"\b" + all_search_words[0]).search(str(key).lower()):
+                    if re.compile(all_search_words[0]).search(str(key).lower()):
+                        found_key = key
+                        search_words = all_search_words[1:]
 
-        for item in view["data"]:
-            found = False
-            search_in = ""
-            for key, value in item.items():
-                
-                if found_view_name == "" and found_key == "" or found_view_name == view_name or found_key == key:
-                    search_in += " " + str(value)
-                    try:
-                        # Suche in Beziehung
-                        foreign_view = relations[view_name][key]
-                        foreign_data = local_views_and_data[foreign_view]["data"]
-                        for d in foreign_data:
-                            if d["ID"] == value:
-                                search_in += " " + str(d["Name"])
-                                break
-                    except KeyError:
-                        pass
+            for item in view["data"]:
+                found = False
+                search_in = ""
+                for key, value in item.items():
 
-                # mit Suche am Wortanfang deaktiviert (12.10.) if all(re.compile(r"\b" + word).search(str(search_in).lower()) for word in search_words):
-                if all(re.compile(word).search(str(search_in).lower()) for word in search_words):
-                    # Wenn alle Wörter im Wert gefunden werden, füge das Element zur Ergebnisliste hinzu
-                    view["data_found"].append(item)
-                    found = True
-                    found_in_view = True
-                    break
+                    if found_view_name == "" and found_key == "" or found_view_name == view_name or found_key == key:
+                        search_in += " " + str(value)
+                        try:
+                            # Suche in Beziehung
+                            foreign_view = relations[view_name][key]
+                            foreign_data = local_views_and_data[foreign_view]["data"]
+                            for d in foreign_data:
+                                if d["ID"] == value:
+                                    search_in += " " + str(d["Name"])
+                                    break
+                        except KeyError:
+                            pass
 
-            if not found:
-                # Wenn das Element oder seine Beziehungstabellen nicht alle Wörter enthalten, überspringen Sie das Element
-                continue
-        if found_in_view:
-            views_found[view_name] = view
-            if view_name == current_view_name:
-                found_in_current_view = True
+                    # mit Suche am Wortanfang deaktiviert (12.10.) if all(re.compile(r"\b" + word).search(str(search_in).lower()) for word in search_words):
+                    if all(re.compile(word).search(str(search_in).lower()) for word in search_words):
+                        # Wenn alle Wörter im Wert gefunden werden, füge das Element zur Ergebnisliste hinzu
+                        view["data_found"].append(item)
+                        found = True
+                        found_in_view = True
+                        break
 
-    if len(views_found) == 0:
-        views_found = local_views_and_data
-        search = ""
+                if not found:
+                    # Wenn das Element oder seine Beziehungstabellen nicht alle Wörter enthalten, überspringen Sie das Element
+                    continue
+            if found_in_view:
+                found_views_and_data[view_name] = view
+                if view_name == current_view_name:
+                    found_in_current_view = True
+
+    if len(found_views_and_data) == 0:
+        found_views_and_data = local_views_and_data
     else:
         if found_in_current_view == False:
-            for view_name, view in views_found.items():
+            for view_name, view in found_views_and_data.items():
                 current_view_name = view_name
                 break
 
-    columnsWidth = {}
-    for key, value in views[view_name]["columns"].items():
-        columnsWidth[key] = 50      
-        # columnsWidth[column_name] = config["columnsWidth"][column_name]     
+    last_view_name = current_view_name
+    last_found_views_and_data = found_views_and_data
+    last_view = view
 
-    return render_template("index.html", view_name=current_view_name, relations=view["relations"], views=views_found, columnsWidth=columnsWidth, search=search)
+    columnsWidth = {}
+    for key, value in views[current_view_name]["columns"].items():
+        columnsWidth[key] = 50
+        # columnsWidth[column_name] = config["columnsWidth"][column_name]
+
+    return render_template("index.html", view_name=current_view_name, relations=view["relations"], views=found_views_and_data, columnsWidth=columnsWidth, search=search)
 
 
 @app.route('/save_data', methods=['POST'])
